0058ded5e297e8d5abaa03fe959a5ba34d831d98
navfal2010 <navfal808@gmail.com>
2024-04-16 19:07:48 +0500

    done

---
+++ main.js
@@ -0,0 +1,70 @@
+// Zadacha-1
+// let first = 58
+// let second = 37
+// console.log(first + second);
+
+
+
+
+// Zadacha-2
+// let a = +'12'
+// console.log(a);
+
+
+
+
+
+
+// Zadacha-3
+// let raqam = (Math.round(Math.random() * 100))
+// alert(`${'Random orqali chiqan son:  '}${raqam}`)
+// if (raqam % 2 === 0) {
+//     alert(`${raqam}${' :juft son'}`)
+// }
+// else {
+//     alert(`${raqam}${' :toq son'}`)
+// }
+
+
+
+
+// Zadacha-4
+// let soz = prompt('Iltimos soz kiriting')
+// let soz2 = soz.toLocaleLowerCase()
+// if (soz2 === 'mars') {
+//     console.log('Mars');
+// }
+// else {
+//     console.log('Mars It School');
+// }
+
+
+
+
+// Zadacha-5
+// let arr = ['noutbuk', 'olma', 'avtomobil', 'soz', 'kitob'];
+// console.log(arr);
+// let result = [];
+// for (let i = 0; i < arr.length; i++) {
+//     result.push(arr.filter((v) => v.length >= 5))
+// }
+// console.log(result);
+
+
+
+
+// Zadacha-6
+// let son = 100
+// for (let i = 1; i <= son; i++) {
+//     if (i % 2 === 0) {
+//         console.log(`${i}${' juft'}`);
+//     }
+//     else {
+//         console.log(`${i}${' toq'}`);
+//     }
+// }
+
+
+
+
+
